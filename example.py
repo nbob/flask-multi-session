f286932d4508082ce2a0a27973f11ac9926743ef
Nikita Bobrov <atkin1450@gmail.com>
2015-02-20 13:39:40 +0300

    example added

---
+++ example.py
@@ -0,0 +1,39 @@
+from flask import Flask, session, redirect, url_for
+import random
+
+app = Flask(__name__)
+app.secret_key = 'SECRET_KEY'
+
+@app.route('/')
+def index():
+  user_id = session.get('user_id')
+  if user_id is not None:
+    return """
+      Hello. This is index page. Your login is %s.</br>
+      <a href="/logout">Logout</a></br>
+      <a href="/logout_all_devices">Logout from all devices</a>
+    """ % user_id
+  else:
+    return 'Hello. This is index page. Please <a href="/login">login</a>.'
+
+@app.route('/login')
+def login():
+  session['user_id'] = random.randint(1, 10000)
+  return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+  del session['user_id']
+  return redirect(url_for('index'))
+
+@app.route('/logout_all_devices')
+def logout_all_devices():
+  session.clear_user_sessions()
+  return redirect(url_for('index'))
+
+from flask_redis_session import RedisSessionInterface
+app.session_interface = RedisSessionInterface()
+
+
+if __name__ == '__main__':
+  app.run()
