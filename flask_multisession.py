28c6dc40e35e3d30fd7f04dbd363092452d64c15
Nikita Bobrov <nb@highloadlab.com>
2018-05-09 13:24:07 +0300

    fix

--- flask_multisession.py
+++ flask_multisession.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-__version_info__ = ('0', '2', '1')
+__version_info__ = ('0', '2', '2')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Nikita Bobrov'
 __license__ = 'MIT/X11'
